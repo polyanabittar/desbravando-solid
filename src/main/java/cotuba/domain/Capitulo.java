dd2928b3ca836f4ca4049ec8e8498a81070a08a9
polybittar <polyana.bittar@zup.com.br>
2023-04-18 13:24:32 -0300

    separando em mvc, pacotes e  módulos

---
+++ src/main/java/cotuba/domain/Capitulo.java
@@ -0,0 +1,23 @@
+package cotuba.domain;
+
+public class Capitulo {
+
+    private String titulo;
+    private String conteudoHTML;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getConteudoHTML() {
+        return conteudoHTML;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setConteudoHTML(String conteudoHTML) {
+        this.conteudoHTML = conteudoHTML;
+    }
+}
